4f472486daea1a589fc6f7f2196935fb0712afc2
sangyeon <asylee02@gmail.com>
2023-12-04 01:32:15 +0900

    디자인 수정

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -16,22 +16,33 @@ export default function Main() {
 
   return (
     <div>
-      <header className='flex w-screen h-24 bg-black bg-opacity-60 items-center justify-evenly fixed'>
-          <div className='w-40 h-12 bg-slate-600'></div>
+      <header className='flex w-screen h-24 bg-black bg-opacity-60 items-center fixed'>
+          <div className='w-28 z-20 ml-72'>
+          {/* https://url.kr/sje5t7  까만거 */}
+          {/* https://url.kr/c81l4s  하얀거 */} 
+            <img src="https://url.kr/c81l4s" alt="" />
+          </div>
           <nav className='w-1/3'>
-              <ul className='flex text-white font-bold text-xl justify-evenly'>
-                  <li>회사소개</li>
-                  <li>제품</li>
-                  <li>문의사항</li>
+              <ul className='flex text-white font-bold text-lg justify-evenly mr-48'>
+                  <li className='hover:text-stone-500 hover:underline cursor-pointer '>ABOUT US</li>
+                  <li className='hover:text-stone-500 hover:underline cursor-pointer '>PRODUCT</li>
+                  <li className='hover:text-stone-500 hover:underline cursor-pointer '>CONTACT</li>
               </ul>
           </nav>
-          <form action="" className='h-9 w-1/6 relative'>
+          <form action="" className='h-9 w-1/6 relative ml-96'>
             <input type="text" placeholder='Search' className='h-9 w-full rounded-full bg-white opacity-70 flex items-center pl-5 justify-between pr-3'/>
             <button><RiSearchLine className='text-3xl absolute left-3/4 top-1 ml-8'/></button>
           </form>
         </header>
       <main>
-        <article className='h-screen bg-cover main_article1'>
+        <article className='h-screen bg-cover main_article1 text-white text-7xl font-bold flex flex-col-reverse justify-center items-center '>
+          <div className='w-screen flex justify-center text-9xl'>DNS Korea</div>
+          <div className='mb-56 flex justify-evenly w-screen mx-36'>
+            <p>Value</p>
+            <p>Innovation</p>
+            <p>Trust</p>
+            <p>Customer</p>
+          </div>
           
         </article>
         <article className='h-4/5 '>
